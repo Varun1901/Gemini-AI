5c8b5e9cc1f42994109d03a2ed3361b34ec3f08c
Varun1901 <165415944+Varun1901@users.noreply.github.com>
2024-08-21 19:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import google.generativeai as genai
 st.title( "Welcome to VARUN-GenAI")
-genai.configure(api_key="AIzaSyC-24pSEnb5JyYJe-_xQ4_vj4LHQNijH-g")
+genai.configure(api_key="API-key")
 text = st.text_input("Enter your prompt!!!")
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
